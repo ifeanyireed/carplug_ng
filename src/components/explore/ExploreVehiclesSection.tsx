3b695c49920f3225757144ef1521317aeba7d960
Ifeanyi Reed <ifeanyireed@ahnara.com>
2026-08-25 16:35:43 +0100

    style: reduce padding between vehicle cards in explore section

--- src/components/explore/ExploreVehiclesSection.tsx
+++ src/components/explore/ExploreVehiclesSection.tsx
@@ -196,7 +196,7 @@ export const ExploreVehiclesSection = ({
         </div>
 
         {/* Vehicles Grid with Tighter Padding/Gap */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3.5 sm:gap-4 lg:gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-2.5 lg:gap-3">
           {filteredVehicles.map((car) => {
             const isFav = !!favorites[car.id];
             return (
